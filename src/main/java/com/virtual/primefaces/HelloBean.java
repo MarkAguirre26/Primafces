78c86c50d183dcdb23bfd57a5bbe7ed424422d53
Koi git <96707504+koiy7010@users.noreply.github.com>
2024-05-29 14:26:54 +0800

    init commit

---
+++ src/main/java/com/virtual/primefaces/HelloBean.java
@@ -0,0 +1,16 @@
+package com.virtual.primefaces;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+
+@RequestScoped
+@Named
+public class HelloBean {
+
+    public String sampleOutPut(){
+        return  "Hi Mark, From bean test jsf";
+    }
+
+
+}
